498e95f648cbb3236312cc1d1a113b47f847c126
Dave Kelly <dkelly@ambientage.com>
2013-01-10 23:32:34 +0000

    re-wrote js, added footer nav

--- js/main.js
+++ js/main.js
@@ -1,60 +1,64 @@
 
 $(document).ready(function(){
 
-	var mockupDir = 'mockups';
-	var firstImage = '';
-	aa.imageCount = aa.mockups.length;
+	var mockupDir 	= 'mockups';
+	var firstImage 	= '';
+	aa.imageCount 	= aa.mockups.length;
 	aa.currentIndex = 0;
 
 	// Setup initial state
-	if( aa.imageCount > 0){
+	if( aa.imageCount > 0)
+	{
 		$('span#imageTotal').text( aa.imageCount );
 		$('span#imageNumber').text( aa.currentIndex + 1 );
 		$('#holder').html('<img src="' + mockupDir + '/' + aa.mockups[aa.currentIndex] + '" />'); 
-		$('#previous').hide();
+		$('.previous').hide();
 	}
 
 	// no nav if single image
-	if( aa.imageCount <= 1){
+	if( aa.imageCount <= 1)
+	{
 		$('nav').hide();
 	}
 
-	$('#next').click(function(){
+	// Prev & Next buttons
+	$('a').click(function()
+	{
 
 		$('.alert').fadeOut('fast');
-		$('#previous').show();
-		var maxImgIndex = aa.imageCount - 1;
 		
-		if( aa.currentIndex < maxImgIndex){			
-			aa.currentIndex++;			
-			$('#holder').find('img').fadeOut('slow');
-			$('#holder').html('<img src="' + mockupDir + '/' + aa.mockups[ aa.currentIndex ] + '" />').css({'display': 'none'}).fadeIn(800); 			
-			var imageNumber = aa.currentIndex + 1;
-			$('span#imageNumber').text( imageNumber );
-		}else{
-			$('#holder').before('<div class="alert alert-error">You\'ve reached the end of the mockups</div>').fadeIn();
-		}
+		var $this = $(this);
 
-		if( aa.currentIndex == maxImgIndex ){
-			$('#next').hide();
-		}
+		if( $this.hasClass('next')){
+			$('.previous').show();
+			var maxImgIndex = aa.imageCount - 1;
+			if( aa.currentIndex < maxImgIndex){			
+				aa.currentIndex++;			
+			}
+			// lose next button if @ end of 
+			if( aa.currentIndex == maxImgIndex ){
+				$('.next').hide();
+			}
+		}else if( $this.hasClass('previous')){
+			$('.next').show();
+			if( aa.currentIndex > 0 ){			
+				aa.currentIndex--;			
+			}
+			// lose prev button if on first
+			if( aa.currentIndex == 0 ){
+				$('.previous').hide();
+			}
+		}	
+
+		// do image transitions
+		$('#holder').find('img').fadeOut('slow');
+		$('#holder').html('<img src="' + mockupDir + '/' + aa.mockups[ aa.currentIndex ] + '" />').css({'display': 'none'}).fadeIn('slow'); 			
+		
+		var imageNumber = aa.currentIndex + 1;
+		$('span#imageNumber').text( imageNumber );
+		$('html, body').animate({scrollTop: $("#wrapper").offset().top}, 800);
+
+		return false;
 	});
 
-	$('#previous').click(function(){
-		$('.alert').fadeOut('fast');
-		$('#next').show();
-		if( aa.currentIndex > 0 ){			
-			aa.currentIndex--;			
-			$('#holder').find('img').fadeOut('slow');
-			$('#holder').html('<img src="' + mockupDir + '/' + aa.mockups[ aa.currentIndex ] + '" />').css({'display': 'none'}).fadeIn('slow'); 			
-			var imageNumber = aa.currentIndex + 1;
-			$('span#imageNumber').text( imageNumber );
-		}else{
-			$('#holder').before('<div class="alert alert-error">No Previous Images to Display</div>').fadeIn();;
-		}
-
-		if( aa.currentIndex == 0 ){
-			$('#previous').hide();
-		}
-	})
 }); 
